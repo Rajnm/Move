45cb920a081a57278eed35dc746903882404cd2d
Rajnm <royalrk@gmail.com>
2017-01-25 11:50:00 +1100

    Setup
    Added files to git

---
+++ ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Move
+//
+//  Created by Raj karthik Nalini mohan on 25/1/17.
+//  Copyright © 2017 Raj karthik Nalini mohan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var emailAddress: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBAction func login(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
